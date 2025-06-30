576cb3f0d6634bada7ad875d3089ee6b9cdb580c
Dhaval Bhimani <dhavalbhimani14@gmail.com>
2025-06-30 13:45:49 +0530

    small ui change

--- components/forms/mailform.tsx
+++ components/forms/mailform.tsx
@@ -4,6 +4,7 @@ import emailjs from "@emailjs/browser";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { toast } from "sonner";
+import { Loader2Icon } from "lucide-react";
 
 interface MailFormProps {
   onSuccess?: (newUser: { email: string }) => void;
@@ -22,7 +23,7 @@ const MailForm = ({ onSuccess }: MailFormProps) => {
     e.preventDefault();
 
     if (!email || !email.includes("@")) {
-      toast.error("Please enter a valid email.");
+      toast.info("Please enter a valid email.");
       return;
     }
 
@@ -53,7 +54,7 @@ const MailForm = ({ onSuccess }: MailFormProps) => {
           process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
         );
 
-        toast.success(`You're successfully added to the waitlist! Your queue number is #${data.queue}`);
+        toast.success("You're successfully added to the waitlist! Check your mail.");
 
         // 🔥 Notify parent component immediately
         if (onSuccess) {
@@ -95,7 +96,7 @@ const MailForm = ({ onSuccess }: MailFormProps) => {
           className={`${loading ? "opacity-50 cursor-not-allowed" : "hover:bg-neutral-800"}`}
         >
           {loading ? (
-            <><div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin" /> <span>Processing</span></>
+            <div className="flex gap-x-1 items-center justify-center"><Loader2Icon className="size-4 animate-spin"/> <span>Processing</span></div>
           ) : (
             "Join Waitlist"
           )}
