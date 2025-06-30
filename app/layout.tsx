06e582d7093f51e1be566decb2840c9f261a888c
Dhruv Patel <99269dhruvpatel@gmail.com>
2025-06-30 13:09:50 +0530

    added toast glowing effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster/>
+        <Toaster toastOptions={{ className: "toast-glow-animated bg-black" }} />
       </body>
     </html>
   );
