0b6c8b946c9a08cbabf18ca064714d13fa067276
Dhaval Bhimani <dhavalbhimani14@gmail.com>
2025-07-05 15:14:03 +0530

    (fix)

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    minimumCacheTTL: 60,
-  },
-  swcMinify: true,
-  reactStrictMode: true,
-  poweredByHeader: false,
+  
 };
 
 export default nextConfig;
